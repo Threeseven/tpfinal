9f85af3cd69a095ebbbd7a51bfefd46e73225f36
dario26 <villarrealdario@gmail.com>
2015-06-19 09:16:11 -0300

    codificador y decodificador

--- dominiofrecuencial.m
+++ dominiofrecuencial.m
@@ -2,7 +2,7 @@
 close all
 clear all
 
-[y, fs, nbits]=wavread('sonido4.wav');
+[y, fs, nbits]=wavread('sonido4');
 subplot(2,1,1)
 %si la señal tiene dos canales me quedo con uno y la acorto
 if size(y,2)>1
@@ -40,7 +40,7 @@ for i=1:length(c_a)
                         th(i)=3;
                         
     else
-        th(i)=3;
+        th(i)=1;
         
     end
 end
@@ -63,6 +63,7 @@ nbits_m=8;%cantidad de para codificar el mensaje
 %mensaje
 mens_temp=dec2bin(single('Estaba el diablo mal parado en la esquina de mi barrio ahi donde dobla el viento y se cruzan los atajos. Al lado de él estaba la muerte, con una botella en la mano me miraban de reojo y se reían por lo bajo.Y yo que esperaba no sé a quién,al otro lado de la calle del otoño una noche de bufanda que me encontró desvelado,entre dientes oí a la muerte que decía así: Cuántas veces se habrá escapado,como laucha por tirantey esta noche que no cuesta nada, ni siquiera fatigarme, podemos llevarnos un cordero, con solo cruzar la calle. Yo me escondí tras la niebla y miré al infinito,a ver si llegaba ese que nunca iba a venir.Estaba el diablo mal parado en la esquina de mi barrio,al lado de él estaba la muerte,con una botella en la mano. Y temblando como una hoja,me crucé para encararlos,y les dije, me parece que esta vez me dejaron bien plantado. Les pedí fuego y del bolsillo saqué una rama pa convidarlos y bajo un árbol del otoñonos quedamos chamuyando,me contaron de sus vidas,sus triunfos y sus fracasos,de que el mundo andaba locoy hasta el cielo fué comprado y más miedo que ellos dos,me daba el propio ser humano.Y yo ya no esperaba a nadie,y entre las risas del aquelarre el diablo y la muerte se me fueron amigando, ahí donde dobla el viento y se cruzan los atajos, ahí donde brinda la vida en la esquina de mi barrio. Estaba el diablo mal parado en la esquina de mi barrio ahi donde dobla el viento y se cruzan los atajos. Al lado de él estaba la muerte, con una botella en la mano me miraban de reojo y se reían por lo bajo.Y yo que esperaba no sé a quién,al otro lado de la calle del otoño una noche de bufanda que me encontró desvelado,entre dientes oí a la muerte que decía así: Cuántas veces se habrá escapado,como laucha por tirantey esta noche que no cuesta nada, ni siquiera fatigarme, podemos llevarnos un cordero, con solo cruzar la calle. Yo me escondí tras la niebla y miré al infinito,a ver si llegaba ese que nunca iba a venir.Estaba el diablo mal parado en la esquina de mi barrio,al lado de él estaba la muerte,con una botella en la mano. Y temblando como una hoja,me crucé para encararlos,y les dije, me parece que esta vez me dejaron bien plantado. Les pedí fuego y del bolsillo saqué una rama pa convidarlos y bajo un árbol del otoñonos quedamos chamuyando,me contaron de sus vidas,sus triunfos y sus fracasos,de que el mundo andaba locoy hasta el cielo fué comprado y más miedo que ellos dos,me daba el propio ser humano.Y yo ya no esperaba a nadie,y entre las risas del aquelarre el diablo y la muerte se me fueron amigando, ahí donde dobla el viento y se cruzan los atajos, ahí donde brinda la vida en la esquina de mi barrio.'),nbits_m);
 mens=[];
+
 %tranformar mensaje en vector fila
 for i=1:size(mens_temp,1)
     mens=[mens mens_temp(i,:)];
@@ -73,13 +74,15 @@ mens_bin=dec2bin(mens,nbits_m);
  k=1;
  i=1;
  j=0;
- tm=length(mens);
+ tm=length(mens)+16;
  while (j<tm)
      j=j+th(i);
      i=i+1;
  end
- i2=i;
 
+ bits_tm=16;
+ tm_bin=dec2bin(tm,bits_tm);
+ mens=[tm_bin(1:8) tm_bin(9:16) mens];
 for j=1:i-2
   ca_bin(j,nbits-th(j)+1:nbits)=mens(k:k+th(j)-1);
   k=k+th(j);
@@ -94,18 +97,27 @@ x=y_dec-minimo;
 subplot(2,1,2)
 plot(x)
 sound(x,fs)
-    
-% verificacion
-
+wavwrite(x,fs,nbits,'salida.wav')
+%% verificacion
+close all
+clear all
+nbits_m=8;
+[y, fs2, nbits2]=wavread('sonido4');
+if size(y,2)==2
+    y=y(1:22560,1);
+end
+[x, fs, nbits]=wavread('salida.wav');
+haarint=liftwave('haar','int2int');
 [ca_ver,cd]=lwt(x,haarint);
 
-%minimo=(min(ca_ver));
+minimo=(min(ca_ver));
+%minimo=0.9592;
 c_b=ca_ver+abs(minimo);
 c_b2=c_b.*(2^(nbits-1));
-th2=zeros(1,length(c_a));
+th2=zeros(1,length(ca_ver));
 bits_fijos=8;
 
-for i=1:length(c_a)
+for i=1:length(ca_ver)
     if ((abs(c_b2(i)))>=2^(nbits-1))
         th2(i)=nbits-1-bits_fijos;
         %th(i)=15-bits_fijos;
@@ -125,18 +137,34 @@ for i=1:length(c_a)
                         th2(i)=3;
                         
     else
-        th2(i)=3;
+        th2(i)=1;
         
     end
 end
-
+%th2=th;
 
 
 c_bin2=dec2bin(c_b2,nbits);
+w=0;
+k=1;
+j=1;
 
+while (k<=16)
+  tm_bin(k:k+th2(j)-1)=c_bin2(j,nbits-th2(j)+1:nbits);
+  k=k+th2(j); 
+  j=j+1;
+end
+i=1;
+j=0;
+tm=bin2dec(tm_bin(1:16));
+ while (j<=tm+16)
+     j=j+th2(i);
+     i=i+1;
+ end
 %m=zeros(size(ms_bin,1),size(ms_bin,2));
 k=1;
-for j=1:i2-2
+
+for j=1:i-2
   mens_re(k:k+th2(j)-1)=c_bin2(j,nbits-th2(j)+1:nbits);
   k=k+th2(j);
 end
@@ -147,7 +175,7 @@ for j=0:round(length(mens_re)/nbits_m)-2
 end
 s=bin2dec(g);
 largo=[length(x) length(y)];
-char(s')    
+char(s(3:(tm/8))')
     
     
 

b8ecdbe1da8e0362396b35f2422d2b0826e293c5
Tranfuga25s <tranfuga25s@gmail.com>
2015-06-22 21:12:06 -0300

    Separado encoders segun metodo

---
+++ lsbBasicEncoder.m
@@ -0,0 +1,29 @@
+function [archivo_salida, snr] = dwtencoder( archivo_entrada, msg )
+  % LSB original para ocultar texto
+  [y, fs, nbits]=wavread(archivo_entrada);
+  if size(y,2)==2;
+      y=y(1:22560,1);
+  end
+  minimo=abs(min(y));
+  y_a=y+minimo;
+  y_a=y_a.*(2^(nbits-1));
+  % nbits=8;
+  y_bin=dec2bin(y_a,nbits);
+
+
+  nbits_m=8; % Cantidad de bits para codificar el mensaje
+  % Mensaje a codificar
+  ms_d=single(msg);
+  ms_bin=dec2bin(ms_d,nbits_m);
+  % Codificación
+  k=1;
+  for i=1:size(ms_bin,1)
+    for j=1:size(ms_bin,2)
+        y_bin(k,nbits)=ms_bin(i,j);
+        k=k+1;
+    end
+  end
+  y_dec=bin2dec(y_bin);
+  y_dec=(y_dec./2^(nbits-1))-minimo;
+  snr= 10*log10((sum(y.^2))/(sum(y-y_dec).^2));
+end
---
+++ lsbStepByteEncoder.m
@@ -0,0 +1,34 @@
+function [archivo_salida, snr] = lsbStepByteEncoder( archivo_entrada, msg )
+
+    [y, fs, nbits]=wavread(archivo_entrada);
+    if size(y,2)>1
+        y=y(1:22560,1);
+    end
+    minimo=abs(min(y));
+    y_a=y+minimo;
+    y_a=y_a.*(2^(nbits-1));
+    y_bin=dec2bin(y_a,nbits);
+
+
+    nbits_m=8;%cantidad de para codificar el mensaje
+    % Mensaje a codificar
+    ms_d=single(msg);
+    ms_bin=dec2bin(ms_d,nbits_m);
+    %Codificación
+    k=1;
+    w=0;
+    lsb=nbits;
+    for i=1:size(ms_bin,1)
+      for j=1:size(ms_bin,2)
+          if k<size(y_bin,1)
+            y_bin(k,lsb)=ms_bin(i,j);
+            w=bin2dec(y_bin(k,1:3));            
+            k=k+w+1;
+          end
+         
+      end
+    end
+    y_dec=bin2dec(y_bin);
+    y_dec=(y_dec./2^(nbits-1))-minimo;
+    snr= 10*log10((sum(y.^2))/(sum(y-y_dec).^2));
+end
